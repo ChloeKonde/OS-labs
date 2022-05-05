a4008c25f57ae3c0874a3ed11b20c2e4236ba9d1
ChloeKonde <konde.chloe@gmail.com>
2022-05-05 14:45:57 +0500

    add first lab

---
+++ laba1/laba1/Program.cs
@@ -0,0 +1,41 @@
+﻿// вариант 4
+using System;
+using System.Diagnostics;
+
+namespace laba1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if(args.Length > 0) 
+			{
+				Console.WriteLine(args[0]);
+				// Console.WriteLine(args[1]);
+
+			}
+
+			Process myProcess = new Process();
+
+			try
+			{
+				myProcess.StartInfo.UseShellExecute = false;
+
+				myProcess.StartInfo.FileName = args[0];
+				myProcess.StartInfo.Arguments = args[1];
+				myProcess.Start();
+
+				Console.WriteLine("Дочерний процесс запущен. Ждём его окончания...");
+
+				myProcess.WaitForExit();
+
+				Console.WriteLine("Дочерний процесс закончен.");
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+	}
+}
